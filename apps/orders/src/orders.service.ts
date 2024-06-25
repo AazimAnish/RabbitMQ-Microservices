675f64ac2d7274a5196245af6ee1e410340e74f6
AazimAnish <aazimanish1@gmail.com>
2024-06-25 13:09:14 +0530

    feat: order created

--- apps/orders/src/orders.service.ts
+++ apps/orders/src/orders.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { OrderRepository } from './orders.repository';
+import { CreateOrderRequest } from './dto/create-order.request';
 
 @Injectable()
 export class OrdersService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly orderRepository: OrderRepository) {}
+
+  async createOrder(request: CreateOrderRequest) {
+    return this.orderRepository.create(request);
   }
 }
